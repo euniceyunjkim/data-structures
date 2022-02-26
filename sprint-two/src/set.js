9a7a311da067d643cdf8544935f42ea6005c1846
Eunice Kim <euniceyunjkim@gmail.com>
2022-02-25 21:16:49 -0800

    Complete set pair sprint assignment

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,18 +1,35 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = null; // fix me
+  set.storage = []; // fix me
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item) {
+//take a string and add to set
+  if (!this.contains(item)) {
+    this.storage.push(item);
+  }
 };
 
 setPrototype.contains = function(item) {
+//takes a string
+  if (this.storage !== [] && this.storage.indexOf(item) > -1) {
+    return true;
+  }
+  //return boolean if found in set
+  return false;
 };
 
 setPrototype.remove = function(item) {
+//takes string
+  if (this.contains(item)) {
+    var index = this.storage.indexOf(item);
+    this.storage.splice(index, 1);
+  }
+  //if found
+  //remove from set
 };
 
 /*
