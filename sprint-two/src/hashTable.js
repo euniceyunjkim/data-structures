428ba081feb3d060985d1f684cd40dab3780b9b3
Eunice Kim <euniceyunjkim@gmail.com>
2022-02-25 23:11:03 -0800

    Complete hashTable pair sprint assignment

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -1,4 +1,4 @@
-
+//pseudoclassical style
 
 var HashTable = function() {
   this._limit = 8;
@@ -7,15 +7,29 @@ var HashTable = function() {
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+
+  if (this._storage.get(index) === undefined) {
+    this._storage.set(index, {});
+  }
+  this._storage.get(index)[k] = v;
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  return this._storage.get(index)[k];
 };
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  var callBackFunc = function(object, index, array) {
+    if (object !== undefined) {
+      delete object[k];
+    }
+
+  };
+  this._storage.each(callBackFunc);
 };
+
 
 
 
